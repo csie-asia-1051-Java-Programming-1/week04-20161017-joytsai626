a2b5877b252a2b46709b5e077321c64381166854
joytsai626 <105021013@live.asia.edu.tw>
2016-10-17 16:49:39 +0800

    ex
    ex

--- src/ex/ex03_105021013.java
+++ src/ex/ex03_105021013.java
@@ -14,7 +14,15 @@ public class ex03_105021013 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
+		System.out.println("輸入n:");
 		int n = scn.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 }
